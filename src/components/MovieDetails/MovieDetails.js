148addb6d1e5dd16a8c84d4a2108721ad5b58f8d
Megan Henry <megangabriellehenry@gmail.com>
2020-03-07 18:47:29 -0600

    single out movie that routes to details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
- 
+import {connect} from 'react-redux';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import { Typography } from '@material-ui/core';
+// import Button from '@material-ui/core/Button';
+
 class MovieDetails extends Component {
+    componentDidMount(){
+        console.log('history', this.props.history);   
+    }
     render() {
+        const movieId = this.props.history.location.pathname
         return (
-            <div className='MovieDetails'>
-                <h1>DETAILS Test</h1>
+            <div className='MovieDetails'> 
+                {/* <h1>{JSON.stringify(movieId.charAt(movieId.length-1))}</h1> */}
+                {/* {JSON.stringify(this.props.reduxState.movies)} */}
+                {this.props.reduxState.movies.map(movie => (movieId.charAt(movieId.length-1) == movie.id)? <h3>YEP</h3>: <></>)}
+
             </div>
         )
     }
 }
-export default MovieDetails;
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+export default connect(putReduxStateOnProps)(MovieDetails);
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -6,33 +6,44 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 class MovieList extends Component {
     componentDidMount(){
         this.getMovies()
     }
     getMovies=()=>{
+        //dispatching to generator fx
         this.props.dispatch({type: 'GET_MOVIES'})
+    }
+    detailsPage=(movie)=>{
+        //routes to movies details page
+        this.props.history.push(`/details/${movie.id}`)
     }
     render() {
         return (
+            //loop thru db results, display on DOM as MaterialUI cards in grid (see css)
             <div className='MovieList'>
                 {this.props.reduxState.movies.map(movie => 
                     <Card className="movieResult" key={movie.id}>
                             <CardMedia 
-                                style={{height: 300}}
+                                style={{height: 400}}
                                 image={movie.poster}
                                 className="moviePoster"
                                 title= {movie.title}
                             />
                         <CardContent>
-                            <Typography>{movie.title}</Typography>
+                            <Typography gutterBottom variant="h5" component="h2">{movie.title}</Typography>
                         </CardContent>
+                        <CardActions>
+                            <Button size="small" variant="outlined" color="primary" onClick={()=>this.detailsPage(movie)}>Learn More</Button>
+                        </CardActions>
                     </Card>)}
             </div>
         )
     }
 }
+//pulling movies from reduxStore
 const putReduxStateOnProps = (reduxState) => ({
     reduxState
 })
